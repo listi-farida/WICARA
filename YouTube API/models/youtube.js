06d194304f5791f2f5e5e93c47ac806832075383
Listi Farida <85052833+listi-farida@users.noreply.github.com>
2022-05-27 20:31:50 +0700

    Delete YouTube API directory

--- YouTube API/models/youtube.js
+++
@@ -1,9 +0,0 @@
-import db from "../config/database.js";
-
-const Youtube = db.define('youtube',{
-    link_youtube: {
-        type: String,
-    },
-});
-
-export default Youtube
--- YouTube API/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { findLinkYoutube } from "../controller/youtube.js";
-
-
-const router = express.Router();
-router.get("/youtube", findLinkYoutube);
-
-export default router;
